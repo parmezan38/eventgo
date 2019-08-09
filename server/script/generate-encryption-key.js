5aba9e873428cebafc7f1ca3c8b571b8a39d3a2c
parmezan38 <luka38.stojanac@gmail.com>
2019-08-09 11:36:57 +0200

    Add vapid key generation to generate-keys script
    - Rename generate-encryption-key script to generate-keys
    - Add vapid key generation to generate-keys

--- server/script/generate-encryption-key.js
+++ server/script/generate-encryption-key.js
@@ -2,6 +2,6 @@ const { randomBytes, secretbox } = require('tweetnacl');
 const { encodeBase64 } = require('tweetnacl-util');
 
 const generateKey = () => encodeBase64(randomBytes(secretbox.keyLength));
-
-console.log('Encryption key:');
+console.log('=======================================\n');
+console.log('Encryption Key:');
 console.log(generateKey());
